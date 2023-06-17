19ed5df1722031cd094237e669ad32ff403ee056
bulenegy <547067717@qq.com>
2023-06-17 20:47:02 +0800

    学习中心

--- app/components/learn.tsx
+++ app/components/learn.tsx
@@ -417,7 +417,7 @@ export function LearnPage() {
           <div>
             <div style={{ 
                   border: 'solid 2px gray', 
-                  width: '20%',
+                  maxWidth: '30%',
                   borderRadius: '10px'
                 }} 
                 onClick= {() =>{window.open('https://mp.weixin.qq.com/s?__biz=Mzg5MTk4Mjg2MA==&mid=2247483776&idx=1&sn=656987bcde8b569f8a945eb3ac122046&scene=19#wechat_redirect', '_blank')}}>

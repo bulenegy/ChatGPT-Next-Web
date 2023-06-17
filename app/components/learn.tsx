b53aecef4a78d9806156b34dce97226370f65774
bulenegy <547067717@qq.com>
2023-06-17 15:52:05 +0800

    Update learn.tsx

--- app/components/learn.tsx
+++ app/components/learn.tsx
@@ -15,106 +15,10 @@ import { useAppConfig, useChatStore } from "../store";
 import { MaskAvatar } from "./mask";
 import { useCommand } from "../command";
 
-function getIntersectionArea(aRect: DOMRect, bRect: DOMRect) {
-  const xmin = Math.max(aRect.x, bRect.x);
-  const xmax = Math.min(aRect.x + aRect.width, bRect.x + bRect.width);
-  const ymin = Math.max(aRect.y, bRect.y);
-  const ymax = Math.min(aRect.y + aRect.height, bRect.y + bRect.height);
-  const width = xmax - xmin;
-  const height = ymax - ymin;
-  const intersectionArea = width < 0 || height < 0 ? 0 : width * height;
-  return intersectionArea;
-}
 
-function MaskItem(props: { mask: Mask; onClick?: () => void }) {
-  return (
-    <div className={styles["mask"]} onClick={props.onClick}>
-      <MaskAvatar mask={props.mask} />
-      <div className={styles["mask-name"] + " one-line"}>{props.mask.name}</div>
-    </div>
-  );
-}
 
-function useMaskGroup(masks: Mask[]) {
-  const [groups, setGroups] = useState<Mask[][]>([]);
 
-  useEffect(() => {
-    const computeGroup = () => {
-      const appBody = document.getElementById(SlotID.AppBody);
-      if (!appBody || masks.length === 0) return;
 
-      const rect = appBody.getBoundingClientRect();
-      const maxWidth = rect.width;
-      const maxHeight = rect.height * 0.6;
-      const maskItemWidth = 120;
-      const maskItemHeight = 50;
-
-      const randomMask = () => masks[Math.floor(Math.random() * masks.length)];
-      let maskIndex = 0;
-      const nextMask = () => masks[maskIndex++ % masks.length];
-
-      const rows = Math.ceil(maxHeight / maskItemHeight);
-      const cols = Math.ceil(maxWidth / maskItemWidth);
-
-      const newGroups = new Array(rows)
-        .fill(0)
-        .map((_, _i) =>
-          new Array(cols)
-            .fill(0)
-            .map((_, j) => (j < 1 || j > cols - 2 ? randomMask() : nextMask())),
-        );
-
-      setGroups(newGroups);
-    };
-
-    computeGroup();
-
-    window.addEventListener("resize", computeGroup);
-    return () => window.removeEventListener("resize", computeGroup);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  return groups;
-}
-
-export function NewChat() {
-  const chatStore = useChatStore();
-  const maskStore = useMaskStore();
-
-  const masks = maskStore.getAll();
-  const groups = useMaskGroup(masks);
-
-  const navigate = useNavigate();
-  const config = useAppConfig();
-
-  const maskRef = useRef<HTMLDivElement>(null);
-
-  const { state } = useLocation();
-
-  const startChat = (mask?: Mask) => {
-    chatStore.newSession(mask);
-    setTimeout(() => navigate(Path.Chat), 1);
-  };
-
-  useCommand({
-    mask: (id) => {
-      try {
-        const mask = maskStore.get(parseInt(id));
-        startChat(mask ?? undefined);
-      } catch {
-        console.error("[New Chat] failed to create chat from mask id=", id);
-      }
-    },
-  });
-
-  useEffect(() => {
-    if (maskRef.current) {
-      maskRef.current.scrollLeft =
-        (maskRef.current.scrollWidth - maskRef.current.clientWidth) / 2;
-    }
-  }, [groups]);
-
-  return (
     <div className={styles["new-chat"]}>
       <div className={styles["mask-header"]}>
         <IconButton
@@ -185,5 +89,3 @@ export function NewChat() {
         ))}
       </div>
     </div>
-  );
-}

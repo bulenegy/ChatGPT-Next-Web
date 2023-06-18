f18d4d22a3cbefa1bb4400672f1913ab72850672
bulenegy <547067717@qq.com>
2023-06-18 18:08:41 +0800

    xuexi

--- app/components/learn.tsx
+++ app/components/learn.tsx
@@ -5,85 +5,47 @@ import "./learn.scss";
 import CloseIcon from "../icons/close.svg";
 import { useNavigate } from "react-router-dom";
 import { FileName, Path } from "../constant";
-import React, { useState, useEffect } from "react";
-
-interface ArticleProps {
-  article: {
-    id: number;
-    name: string;
-    link: string;
-    image: string;
-  };
-}
-
-const Article: React.FC<ArticleProps> = ({ article }) => {
-  const handleClick = () => {
-    window.open(article.link, "_blank");
-  };
-
-  return (
-    <div className="article" onClick={handleClick}>
-      <img src={article.image} alt="Article Image" />
-      <a>{article.name}</a>
-    </div>
-  );
-};
-
+import React, { useState, useEffect } from 'react';
+import articleList from './article';
 
 export function LearnPage() {
   const navigate = useNavigate();
-  const [articles, setArticles] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch("/article.json");
-      const data = await response.json();
-      setArticles(data);
-    };
-    fetchData();
-  }, []);
-
   return (
-
     <ErrorBoundary>
       <div className={styles["mask-page"]}>
         <div className="window-header">
           <div className="window-header-title">
             <div className="window-header-main-title">
               学习中心
-
+              {/* {Locale.Mask.Page.Title} */}
             </div>
             <div className="window-header-submai-title">
               学习更多ChatGPT使用技巧
-
+              {/* {Locale.Mask.Page.SubTitle(allMasks.length)} */}
             </div>
           </div>
+
           <div className="window-actions">
             <div className="window-action-button">
               <IconButton
-                icon={
-                  <CloseIcon />}
+                icon={<CloseIcon />}
                 bordered
                 onClick={() => navigate(Path.Home)}
               />
             </div>
           </div>
         </div>
-        <div style={{ padding: "1rem", overflow: "scroll" }}>
+        <div style={{ padding: '1rem', overflow: 'scroll' }}>
           <div className="LearnConten">
-            {articles.map((article: {
-                            id: number;
-                            name: string;
-                            link: string;
-                            image: string;
-                          }) => (
-              <Article key={article.id} article={article} />
+            {articleList.map((article) => (
+              <div key={article.id} className="article" onClick={() => window.open(article.link, '_blank')}>
+                <img src={article.image} alt="Article Image" />
+                <a>{article.name}</a>
+              </div>
             ))}
-
           </div>
         </div>
       </div>
     </ErrorBoundary>
   );
 }
-
